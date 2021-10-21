dafd707270c02a476253a80c5d4ac0c67d9cc9f9
Madhin <madhinhussain@gmail.com>
2021-10-21 18:37:39 +0100

    fundamentals done

--- setup/app.js
+++ setup/app.js
@@ -1,1 +1,11 @@
-console.log('Typescript');
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Madhin';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occured!', 500);
--- setup/app.ts
+++ setup/app.ts
@@ -1,1 +1,17 @@
-console.log('Typescript');
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Madhin'
+
+if (typeof userInput === 'string') {
+  userName = userInput;
+}
+
+function generateError(message: string, code: number) {
+  throw {message: message, errorCode: code}
+}
+
+generateError('An error occured!', 500);
+
+
